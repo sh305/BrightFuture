70b40c934efaec8961f6d706d985a721d9cf094b
sh305 <shivamraie09@gmail.com>
2026-09-23 16:41:59 +0530

    update

--- EmailApi/Controllers/ContactController.cs
+++ EmailApi/Controllers/ContactController.cs
@@ -11,7 +11,7 @@ public class ContactController(
     ILogger<ContactController> logger,
     IWebHostEnvironment environment) : ControllerBase
 {
-    // Post Methods
+    // Post Method Data
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] ContactRequest request)
     {
